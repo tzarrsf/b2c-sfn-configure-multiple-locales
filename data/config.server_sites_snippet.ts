52d9dc71ab93bf6518d287b7f1a2e9f6894b6313
Tom Zarr <tzarr@salesforce.com>
2026-06-08 08:32:28 -0600

    Cleaned up locales and currencies in typescript example

--- data/config.server_sites_snippet.ts
+++ data/config.server_sites_snippet.ts
@@ -4,27 +4,27 @@ sites: [
         defaultLocale: 'en-US',
         defaultCurrency: 'USD',
         supportedLocales: [
-            {
-                id: 'en-GB',
-                preferredCurrency: 'GBP',
-            },
-            {
-                id: 'fr-FR',
-                preferredCurrency: 'EUR',
-            },
-            {
-                id: 'it-IT',
-                preferredCurrency: 'EUR',
-            },
-            {
-                id: 'ja-JP',
-                preferredCurrency: 'JPY',
-            },
-            {
-                id: 'zh-CN',
-                preferredCurrency: 'CNY',
-            },
-        ],
-        supportedCurrencies: ['EUR', 'GBP', 'JPY'],
+                            {
+                                id: 'en-US',
+                                preferredCurrency: 'USD',
+                            },
+                            {
+                                id: 'en-GB',
+                                preferredCurrency: 'GBP',
+                            },
+                            {
+                                id: 'ja-JP',
+                                preferredCurrency: 'JPY',
+                            },
+                            {
+                                id: 'fr-FR',
+                                preferredCurrency: 'EUR',
+                            },
+                            {
+                                id: 'it-IT',
+                                preferredCurrency: 'EUR',
+                            },
+                        ],
+        supportedCurrencies: ['USD', 'EUR', 'GBP', 'JPY'],
     },
 ],
