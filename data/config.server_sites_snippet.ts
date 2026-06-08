eddedc23c921ae1f0c928a339583d3f000c3e431
Tom Zarr <tzarr@salesforce.com>
2026-06-08 08:21:19 -0600

    Added sites snippet for config.server.ts

---
+++ data/config.server_sites_snippet.ts
@@ -0,0 +1,30 @@
+sites: [
+    {
+        id: 'MarketStreet',
+        defaultLocale: 'en-US',
+        defaultCurrency: 'USD',
+        supportedLocales: [
+            {
+                id: 'en-GB',
+                preferredCurrency: 'GBP',
+            },
+            {
+                id: 'fr-FR',
+                preferredCurrency: 'EUR',
+            },
+            {
+                id: 'it-IT',
+                preferredCurrency: 'EUR',
+            },
+            {
+                id: 'ja-JP',
+                preferredCurrency: 'JPY',
+            },
+            {
+                id: 'zh-CN',
+                preferredCurrency: 'CNY',
+            },
+        ],
+        supportedCurrencies: ['EUR', 'GBP'],
+    },
+],
