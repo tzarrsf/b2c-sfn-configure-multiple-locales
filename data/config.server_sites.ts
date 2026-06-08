be4aadf7aaed8b35e7d39111c044698f02e0b9b5
Tom Zarr <tzarr@salesforce.com>
2026-06-08 15:56:04 -0600

    Removed RefArch from sites typescript snippet

--- data/config.server_sites.ts
+++ data/config.server_sites.ts
@@ -1,25 +1,5 @@
 sites: [
         {
-            id: "RefArchGlobal",
-            defaultLocale: "en-US",
-            defaultCurrency: "USD",
-            supportedLocales: [
-                {
-                    id: "en-GB",
-                    preferredCurrency: "GBP",
-                },
-                {
-                    id: "fr-FR",
-                    preferredCurrency: "EUR",
-                },
-                {
-                    id: "it-IT",
-                    preferredCurrency: "EUR",
-                },
-            ],
-            supportedCurrencies: ["USD", "GBP", "EUR"],
-        },
-        {
             id: "MarketStreet",
             defaultLocale: "en-US",
             defaultCurrency: "USD",
