02c5432d8676600c79ed4c52e26a27587fffb639
Tom Zarr <tzarr@salesforce.com>
2026-06-08 08:35:50 -0600

    Added top-level commerce node for clarity.

--- data/config.server_sites_snippet.ts
+++ data/config.server_sites_snippet.ts
@@ -1,30 +1,33 @@
-sites: [
-    {
-        id: 'MarketStreet',
-        defaultLocale: 'en-US',
-        defaultCurrency: 'USD',
-        supportedLocales: [
-                            {
-                                id: 'en-US',
-                                preferredCurrency: 'USD',
-                            },
-                            {
-                                id: 'en-GB',
-                                preferredCurrency: 'GBP',
-                            },
-                            {
-                                id: 'ja-JP',
-                                preferredCurrency: 'JPY',
-                            },
-                            {
-                                id: 'fr-FR',
-                                preferredCurrency: 'EUR',
-                            },
-                            {
-                                id: 'it-IT',
-                                preferredCurrency: 'EUR',
-                            },
-                        ],
-        supportedCurrencies: ['USD', 'EUR', 'GBP', 'JPY'],
-    },
-],
+commerce: {
+    sites: [
+        {
+            id: 'MarketStreet',
+            defaultLocale: 'en-US',
+            defaultCurrency: 'USD',
+            supportedLocales: [
+                                {
+                                    id: 'en-US',
+                                    preferredCurrency: 'USD',
+                                },
+                                {
+                                    id: 'en-GB',
+                                    preferredCurrency: 'GBP',
+                                },
+                                {
+                                    id: 'ja-JP',
+                                    preferredCurrency: 'JPY',
+                                },
+                                {
+                                    id: 'fr-FR',
+                                    preferredCurrency: 'EUR',
+                                },
+                                {
+                                    id: 'it-IT',
+                                    preferredCurrency: 'EUR',
+                                },
+                            ],
+            supportedCurrencies: ['USD', 'EUR', 'GBP', 'JPY'],
+        },
+        // ... other sites
+    ],
+},
